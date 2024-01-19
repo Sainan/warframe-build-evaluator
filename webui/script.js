edcdcf2dc090a9e002c9a63d4e9bb4f51675cd80
Sainan <sainan@calamity.gg>
2024-01-19 13:53:06 +0100

    Web UI: Show powersuit speed in the more common way with mps in parens

--- webui/script.js
+++ webui/script.js
@@ -395,8 +395,16 @@ function update_evaluation(outbuild)
 		add_stat(ul, outbuild.powersuit.ability_efficiency * 100, "{}% Ability Efficiency");
 		add_stat(ul, outbuild.powersuit.ability_range * 100, "{}% Ability Range");
 		add_stat(ul, outbuild.powersuit.ability_strength * 100, "{}% Ability Strength");
-		add_stat(ul, outbuild.powersuit.movement_speed_mps, "{}m/s Movement Speed")
-		add_stat(ul, outbuild.powersuit.sprint_speed_mps, "{}m/s Sprint Speed")
+		{
+			let li = document.createElement("li");
+			li.textContent = outbuild.powersuit.movement_speed.toFixed(2) + " Movement Speed (" + outbuild.powersuit.movement_speed_mps.toFixed(1) + "m/s)";
+			ul.appendChild(li);
+		}
+		{
+			let li = document.createElement("li");
+			li.textContent = outbuild.powersuit.sprint_speed.toFixed(2) + " Sprint Speed (" + outbuild.powersuit.sprint_speed_mps.toFixed(1) + "m/s)";
+			ul.appendChild(li);
+		}
 		evalbody.appendChild(ul);
 	}
 
